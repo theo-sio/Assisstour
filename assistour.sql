f830a3126e51c50c29061e5f6f3fcee0f4ff7a23
theo <theo.jean.sio@gmail.com>
2021-10-12 16:46:47 +0200

    debut du projet et visualisation

---
+++ assistour.sql
@@ -0,0 +1,37 @@
+drop database if exists assistour;
+create database assistour;
+
+use assistour;
+
+create table TechnicienAssistance(
+);
+
+create table Garage(
+);
+
+create table Partenaire(
+);
+
+create table Sinistre(
+);
+
+create table TypeContrat(
+);
+
+create table Vehicule(
+);
+
+create table Contrat(
+);
+
+create table Adherent(
+);
+
+create table Hotel(
+);
+
+create table Nuité(
+);
+
+create table Rapatriement(
+);
